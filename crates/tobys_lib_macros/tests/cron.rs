c30fdb21e87439c7fbc6551189682485b3a25e9a
Toby <toby@tobysch.eu>
2026-09-05 11:51:04 +0200

    fix:ci and slow test doesn't need to test manual_tick

--- crates/tobys_lib_macros/tests/cron.rs
+++ crates/tobys_lib_macros/tests/cron.rs
@@ -1,45 +1,27 @@
-#![allow(
-    clippy::pedantic,
-    clippy::tests_outside_test_module,
-    clippy::panic_in_result_fn
-)]
+#![expect(clippy::tests_outside_test_module)]
 
 #[tokio::test]
 #[cfg(feature = "cron")]
-async fn test_cron() -> Result<(), ()> {
-    use std::time::Duration;
-
-    use futures::future::join_all;
-    use tobys_lib::{
-        create_cron_jobs, create_cron_time,
-        cron::{CronTime, Scheduler},
-    };
-    use tokio::time::sleep;
+async fn test_cron() {
+    use tobys_lib::{create_cron_jobs, create_cron_time, cron::CronTime};
 
     let every_minute = create_cron_time!(* * * * *);
-    if every_minute != CronTime::new("* * * * *")? {
-        return Err(());
-    }
+    assert_eq!(Ok(every_minute), CronTime::new("* * * * *"));
 
-    let mut scheduler = Scheduler::new_with_jobs(create_cron_jobs!(
-        * * * * * move || {
-            Box::pin(async {
-                println!("test 1!");
-            })
-        };
-        * * * * * move || {
-            Box::pin(async {
-                println!("test 2!");
-            })
-        }
-    ))
-    .init();
-
-    sleep(Duration::from_secs(60)).await;
-
-    let jobs = scheduler.manual_tick();
-    assert_eq!(jobs.len(), 2);
-    join_all(jobs).await;
-
-    Ok(())
+    assert_eq!(
+        create_cron_jobs!(
+            * * * * * move || {
+                Box::pin(async {
+                    println!("test 1!");
+                })
+            };
+            * * * * * move || {
+                Box::pin(async {
+                    println!("test 2!");
+                })
+            }
+        )
+        .len(),
+        2
+    );
 }
